e9a2939aad1c9dd529e973c1a8201baeb247eb20
srini <srinivasiyerb@gmail.com>
2014-03-05 12:03:27 +0530

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+		 System.out.println( "Build when change is pushed to github!" );
+		  System.out.println( "Hello World!" );
     }
 }
