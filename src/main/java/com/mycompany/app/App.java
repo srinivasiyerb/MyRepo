bc4e5a551cb01672058919f66eecc4367203b056
srini <srinivasiyerb@gmail.com>
2014-06-17 12:01:35 +0530

    test4

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-		int i;
+		
 	 System.out.println( "Hello World!" );
 		System.out.println( "integration of github and jenkins" );
 		
